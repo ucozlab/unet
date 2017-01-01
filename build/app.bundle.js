875f2626ee78be2d784f9000429f792b129878e9
ucozlab <la-3111@mail.ru>
2017-01-01 21:10:15 +0200

    task 1 done v.1

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -48,13 +48,55 @@
 
 	(function () {
 
-	    function intersect() {}
+	    var startTimer = performance.now();
 
-	    var res = intersect([1, 2, 3], [3, 4, 5, 6, 2], [2, 4, 8, 9, 2, 3]);
+	    function intersect() {
+	        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	            args[_key] = arguments[_key];
+	        }
 
-	    // res => [2,3] // 2 и 3 - Есть во всех трех массивах
+	        var resultArray = [],
+	            arrays = args.filter(function (argument) {
+	            return Array.isArray(argument);
+	        }),
+	            // mb it is not an Array ?
+	        firstArray = removeDuplicates(arrays[0]);
 
-	    console.log('res:', res);
+	        firstArray.forEach(function (value) {
+
+	            var i = 0;
+
+	            while (i < arrays.length && searchValueInNextArray(value, i)) {
+	                // if has 2 or > consilience search further
+
+	                if (i === arrays.length - 1) {
+	                    return resultArray.push(value);
+	                }
+
+	                i++;
+	            }
+	        });
+
+	        function searchValueInNextArray(value, index) {
+	            return arrays[index].indexOf(value) != -1;
+	        }
+
+	        function removeDuplicates(array) {
+	            return array.filter(function (item, index) {
+	                return array.indexOf(item) == index;
+	            });
+	        }
+
+	        return resultArray;
+	    }
+
+	    var res = intersect([3, 4, 5, 6, 2, 2], [1, 2, 3], [2, 4, 8, 9, 2, 3, 3, 3, 4]);
+
+	    console.log('result:', res);
+
+	    var stopTimer = performance.now();
+
+	    console.log('It took ' + (stopTimer - startTimer) + ' ms.');
 	})();
 
 /***/ }
--- main.js
+++ main.js
@@ -2,16 +2,48 @@
 
 (function () {
 
-    function intersect() {
+    let startTimer = performance.now();
 
+    function intersect(...args) {
 
+        let resultArray = [],
+            arrays      = args.filter((argument) => Array.isArray(argument)),   // mb it is not an Array ?
+            firstArray  = removeDuplicates(arrays[0]);
+
+        firstArray.forEach((value) => {
+
+            let i = 0;
+
+            while (i < arrays.length && searchValueInNextArray(value, i)) { // if has 2 or > consilience search further
+
+                if (i === arrays.length - 1) {
+                    return resultArray.push(value);
+                }
+
+                i++;
+
+            }
+
+        });
+
+        function searchValueInNextArray(value, index) {
+            return arrays[index].indexOf(value) != -1
+        }
+
+        function removeDuplicates(array) {
+            return array.filter((item,index) => array.indexOf(item) == index);
+        }
+
+        return resultArray;
 
     }
 
-    let res = intersect([1,2,3],[3,4,5,6,2],[2,4,8,9,2,3]);
+    let res = intersect([3,4,5,6,2,2],[1,2,3],[2,4,8,9,2,3,3,3,4]);
 
-    // res => [2,3] // 2 и 3 - Есть во всех трех массивах
+    console.log('result:', res);
 
-    console.log('res:', res);
+    let stopTimer = performance.now();
+
+    console.log('It took ' + (stopTimer - startTimer) + ' ms.');
 
 })();
